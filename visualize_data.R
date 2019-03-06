9fff13d9d40f0702d676b6f01b8ca65cc97ccfb0
Yuji Jeong <yjeon009@uottawa.ca>
2019-03-05 23:35:05 -0500

    Tuesday update

--- visualize_data.R
+++ visualize_data.R
@@ -1,7 +1,51 @@
-# data.frame function throws an error if the lengths of the two vectors are not the same
-# So add NA to the shorter vector if needed
+# Creating data.frame for an individual set of observation
+final_resting <- data.frame("duration" = final_resting, "state" = c(rep("resting", times = length(final_resting))))
+final_feeding <- data.frame("duration" = final_feeding, "state" = c(rep("feeding", times = length(final_feeding))))
+final <- rbind(final_resting, final_feeding)
 
-final <- data.frame(x=c(final_resting), y=c(final_feeding))
-colnames(final) <- c("resting","feeding")
+final$"condition" <- c(rep("fed", times = nrow(final)))
+final$"date" <- c(rep("07/27", times = nrow(final)))
+final$"fly" <- c(rep(6, times = nrow(final)))
 
-boxplot(final)
+
+# Collect all final results of all sets of  observation into one data frame called 'compare_boxes'
+compare_boxes <- data.frame()
+compare_boxes <- rbind(compare_boxes, final)
+
+
+# Once all data collected, prepare to create boxplots
+compare_boxes$state <- as.factor(compare_boxes$state)
+compare_boxes$fly <- as.factor(compare_boxes$fly)
+compare_boxes$condition <- as.factor(compare_boxes$condition)
+
+# Creating boxplots for all observation
+if (!"ggplot2" %in% installed.packages()){
+  install.packages("ggplot2")
+}
+library(ggplot2)
+library(RColorBrewer)
+b <- ggplot(compare_boxes, aes(y = duration, x = state, colour = state)) + 
+  geom_boxplot(fill = "#4271AE", alpha = 0.7, notch = FALSE) +
+  scale_y_continuous(name = "Duration (seconds)", limits = c(0, 1250)) +
+  ggtitle("Comparing Resting and Feeding Patterns") + 
+  theme_bw() +
+  theme(plot.title = element_text(size = 12, family = "Tahoma", face = "bold", hjust = 0.5),
+        text = element_text(size = 10, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 10)) +
+  geom_jitter() + 
+  facet_grid(. ~ condition) +
+  scale_fill_brewer(type = "seq")
+  
+b
+
+
+# Confirm observations with summary of data
+fed <- compare_boxes[which(compare_boxes$condition == "fed"), ]
+starved <- compare_boxes[which(compare_boxes$condition == "starved"), ]
+summary(fed[which(fed$state == "feeding"), ]$duration )
+summary(fed[which(fed$state == "resting"), ]$duration )
+summary(starved[which(starved$state == "feeding"), ]$duration )
+summary(starved[which(starved$state == "resting"), ]$duration )
+
+
