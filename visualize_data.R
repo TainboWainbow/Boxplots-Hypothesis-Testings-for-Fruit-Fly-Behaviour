c31ce9b0caeced63c1beecf806c797d9387bdadb
Yuji Jeong <yjeon009@uottawa.ca>
2019-03-07 00:41:40 -0500

    Wednesday update

--- visualize_data.R
+++ visualize_data.R
@@ -1,3 +1,14 @@
+if ("ggplot2" %in% installed.packages()){
+  install.packages("ggplot2")
+}
+if (!"car" %in% installed.packages()){
+  install.packages("car")
+}
+library(ggplot2)
+library(car)
+
+#######################################################################################################################
+
 # Creating data.frame for an individual set of observation
 final_resting <- data.frame("duration" = final_resting, "state" = c(rep("resting", times = length(final_resting))))
 final_feeding <- data.frame("duration" = final_feeding, "state" = c(rep("feeding", times = length(final_feeding))))
@@ -19,11 +30,6 @@ compare_boxes$fly <- as.factor(compare_boxes$fly)
 compare_boxes$condition <- as.factor(compare_boxes$condition)
 
 # Creating boxplots for all observation
-if (!"ggplot2" %in% installed.packages()){
-  install.packages("ggplot2")
-}
-library(ggplot2)
-library(RColorBrewer)
 b <- ggplot(compare_boxes, aes(y = duration, x = state, colour = state)) + 
   geom_boxplot(fill = "#4271AE", alpha = 0.7, notch = FALSE) +
   scale_y_continuous(name = "Duration (seconds)", limits = c(0, 1250)) +
@@ -43,9 +49,36 @@ b
 # Confirm observations with summary of data
 fed <- compare_boxes[which(compare_boxes$condition == "fed"), ]
 starved <- compare_boxes[which(compare_boxes$condition == "starved"), ]
-summary(fed[which(fed$state == "feeding"), ]$duration )
-summary(fed[which(fed$state == "resting"), ]$duration )
-summary(starved[which(starved$state == "feeding"), ]$duration )
-summary(starved[which(starved$state == "resting"), ]$duration )
+fed_feeding <- fed[which(fed$state == "feeding"), ]
+fed_resting <- fed[which(fed$state == "resting"), ]
+stv_feeding <- starved[which(starved$state == "feeding"), ]
+stv_resting <- starved[which(starved$state == "resting"), ]
+
+summary(fed_feeding$duration)
+summary(fed_resting$duration)
+summary(stv_feeding$duration)
+summary(stv_resting$duration)
 
 
+# Comparing durations between factors - Analysis of variance
+# First comparing between the conditions (starved or fed) of the flies
+summary(aov(duration ~ condition, data = compare_boxes))
+
+# Now comparing between the states (feeding or resting)
+summary(aov(duration ~ state, data = compare_boxes))
+
+
+# We find that there is indeed significant difference between factors
+# ANOVA test assumes that the data is normally distributed and the variance across factors are homogenous
+# We check these assumptions:
+# 1. Normality - Normal Q-Q plot
+plot(aov(duration ~ condition, data = compare_boxes), main = "By condition of flies", 2)
+plot(aov(duration ~ state, data = compare_boxes), main = "By state of flies", 2)
+
+# 2. Homogeneity of variance - Residuals vs Fitted plot
+plot(aov(duration ~ condition, data = compare_boxes), main = "By condition of flies", 1)
+plot(aov(duration ~ state, data = compare_boxes), main = "By state of flies", 1)
+
+# 2. Homogeneity of variance - Levene's Test
+leveneTest(duration ~ condition, data = compare_boxes)
+leveneTest(duration ~ state, data = compare_boxes)
