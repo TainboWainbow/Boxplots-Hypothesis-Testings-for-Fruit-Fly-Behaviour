09c91b419004f19fe6fed5aa5701874a71007556
Yuji Jeong <yjeon009@uottawa.ca>
2019-03-12 21:44:39 -0400

    semi-final. Requires last reviewing

--- analyze_data.R
+++ analyze_data.R
@@ -1,13 +1,17 @@
 if (!"dplyr" %in% installed.packages()){
   install.packages("dplyr")
 }
-library(dplyr)
+if (!"car" %in% installed.packages()){
+  install.packages("car")
+}
+library(car)
 
 ############################################################################################
-
 # Summary statistics
 feeding <- compare_boxes[which(compare_boxes$state == "feeding"), ]
 resting <- compare_boxes[which(compare_boxes$state == "resting"), ]
+feeding$condition <- as.factor(feeding$condition)
+resting$condition <- as.factor(resting$condition)
 
 group_by(feeding, condition) %>%
   summarise(
@@ -22,20 +26,26 @@ group_by(resting, condition) %>%
     sd = sd(duration, na.rm = TRUE)
   )
 
-# One-Way ANOVA Test
+############################################################################################
+# Testing for Normality and Homogeneity of Variance
+
+# Normality: Shaprio-Wilk Test
+residuals(feed.aov) %>% shapiro.test()
+residuals(rest.aov) %>% shapiro.test()
+
+# Homogeneity: Levene's Test
+leveneTest(duration ~ condition, data = feeding)
+leveneTest(duration ~ condition, data = resting)
+
+# Non-parametric Alternative for ANOVA: Kruskal-Wallis Rank Sum Test
+kruskal.test(duration ~ condition, data = feeding)
+kruskal.test(duration ~ condition, data = resting)
+
+# One-Way ANOVA Test gives different values but 
+# also rejects H0 for the feeding intervals data
 feed.aov <- aov(duration ~ condition, data = feeding)
 rest.aov <- aov(duration ~ condition, data = resting)
 
 summary(feed.aov)
 summary(rest.aov)
-
-############################################################################################
-# Testing for Normality and Homogeneity of Variance
-
-# Normality: Shaprio-Wilk Test
-
-
-
-
-
 
